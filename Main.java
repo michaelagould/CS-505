843d993a17ba481690c31e886395d5dfe41456e2
mike <michael.a.gould@gmail.com>
2017-10-21 01:39:29 -0400

    Cleaning up and encapsulating

--- Main.java
+++ Main.java
@@ -59,57 +59,67 @@ public class Main {
             }
         }
     }
-}
 
 
-public class Rational extends Number implements Comparable<Rational>{
+	public class Rational extends Number implements Comparable<Rational>{
 	
-	private int numerator;
-	private int denominator;
-	private int gcd;
+		private int numerator;
+		private int denominator;
+		private int gcd;
 	
-	public double doubleValue(){return numerator/denominator;}
-	public float floatValue(){return numerator/denominator;}
-	public int intValue(){return numerator/denominator;}
-	public long longValue(){return numerator/denominator;}
+		public double doubleValue(){return numerator/denominator;}
+		public float floatValue(){return numerator/denominator;}
+		public int intValue(){return numerator/denominator;}
+		public long longValue(){return numerator/denominator;}
 	
-	public Rational(int numerator, int denominator){
+		public Rational(int numerator, int denominator){
 
-		int gcd = gcd(numerator,denominator);
-		this.numerator = numerator/gcd;
-		this.denominator = denominator/gcd;
-	}
+			int gcd = gcd(numerator,denominator);
+			this.numerator = numerator/gcd;
+			this.denominator = denominator/gcd;
+		}
 
-	private int gcd(int first, int second){
-		if (second == 0)
-			return first;
-		return gcd(second, first%second);
-	}
+		private int gcd(int first, int second){
+			if (second == 0)
+				return first;
+			return gcd(second, first%second);
+		}
 		
-	private Rational makeDenominatorSame(Rational o){
+		private Rational makeDenominatorSame(Rational o){
 		
-		if(this.denominator == o.denominator)
-			return this;
-		return new Rational(o.denominator * numerator, o.denominator * denominator);
-	}
+			if(this.denominator == o.denominator)
+				return this;
+			return new Rational(o.denominator * numerator, o.denominator * denominator);
+		}
 		
-	public Rational add(Rational o){
+		public Rational add(Rational o){
 		
-		Rational tempA = this.makeDenominatorSame(o);
-		Rational tempB = o.makeDenominatorSame(this);
-		return new Rational(tempA.numerator + tempB.numerator, tempA.denominator);
-	}
+			Rational tempA = this.makeDenominatorSame(o);
+			Rational tempB = o.makeDenominatorSame(this);
+			return new Rational(tempA.numerator + tempB.numerator, tempA.denominator);
+		}
+
+
+		public Rational sub(Rational o){
+
+			Rational tempA = this.makeDenominatorSame(o);
+			Rational tempB = o.makeDenominatorSame(this);
+			return new Rational(tempA.numerator - tempB.numerator, tempA.denominator);
+		}
+
+		public Rational mul(Rational o){
+
+			return new Rational(numerator * o.numerator, denominator * o.denominator);
+		}
+
+		public Rational div(Rational o){
 	
+		return new Rational(numerator * o.denominator, denominator * o.numerator);
+		}
 
-	public Rational sub(Rational o){
-
-		Rational tempA = this.makeDenominatorSame(o);
-		Rational tempB = o.makeDenominatorSame(this);
-		return new Rational(tempA.numerator - tempB.numerator, tempA.denominator);
-	}
-
-	public Rational mul(Rational o){
-
-		return new Rational(numerator * o.numerator, denominator * o.denominator);
+		public String toString(){
+		
+		return "(" + numerator + "/" + denominator + ")";
+		}
 	}
 }
