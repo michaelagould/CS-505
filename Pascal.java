1565424159244384b0adcb7088a8add187295b29
Michael Gould <michael.a.gould@gmail.com>
2017-11-05 22:47:50 -0500

    Pascal submission

--- Pascal.java
+++ Pascal.java
@@ -1,20 +1,3 @@
-public class Main {
-
-    public static void main(String[] args) {
-
-        int n = args.length == 1 ? Integer.parseInt(args[0]) : 1;
-
-        for (int i = 1; i <= n; ++i) {
-            int[] arr = Pascal.triangle(i);
-            System.out.print((i < 10 ? " " : "") + i + ": ");
-            for (int j : arr) {
-                System.out.print(j + " ");
-            }
-            System.out.println();
-        }
-    }
-}
-
 class Pascal {
 
     public static int[] triangle(int n) {
