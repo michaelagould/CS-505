6755992571dbcf21106a2c0ea434c0dfceddbf95
Michael Gould <michael.a.gould@gmail.com>
2017-11-05 22:46:34 -0500

    Pascal submission

--- Pascal.java
+++ Pascal.java
@@ -20,16 +20,17 @@ class Pascal {
     public static int[] triangle(int n) {
 
         if(n == 1)
-            return new int[1]{1};
+            return new int[]{1};
 
-        int[] result = triangle(n-1);
-        int[] other = new int[n];
+        int[] other = triangle(n-1);
+        int[] result = new int[n];
         result[0] = 1;
-        results[n-1] = 1;
-
-        for(int i = 1; i < n; i++)
+        
+        for(int i = 1; i < n-1; i++)
             result[i] = other[i] + other[i-1];
-
+        
+        result[n-1] = 1;
+        
         return result;
     }
 }
