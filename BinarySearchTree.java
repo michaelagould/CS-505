7a2afd514e15687611b4fc941cc6e256f0ebb20c
Michael Gould <michael.a.gould@gmail.com>
2017-12-07 01:34:05 -0500

    Iterator and insert done, starting remove

--- BinarySearchTree.java
+++ BinarySearchTree.java
@@ -6,29 +6,22 @@ public class BinarySearchTree<E extends Comparable<? super E>> extends BinaryTre
 		returns new Iterator<E>(){
 			
 			public boolean hasNext(){
-				return curr.left != null && curr.right != null;
+				return curr.right != null;
 			}
 			
 			public E next(){
-				return ;
+				Node temp = new Node<E>(curr)
+				if(temp.left == null)	
+					return curr;
+				else
+					curr = this.left.next();
 			}
 			
 			public void remove() {
 			throw new UnsupportedOperationException();
 			}
 			
-			public int getSize(Node<E> curr) {
-				int count = 0;
-				if (curr != null) {
-					count++;
-					getSize(curr.left);
-					getSize(curr.right);
-				}
-			return count;
-		}
-			
 			private Node curr = root;
-			private array[] data = new array[this.getSize(curr)];
 		}
 	}
 	
@@ -38,44 +31,43 @@ public class BinarySearchTree<E extends Comparable<? super E>> extends BinaryTre
 	}
 	
 	private Node<E> insert(Node<E> curr, Node<E> node) {
-		return Node<E>
+		int cmp;
+		if(curr.empty())
+			return node;
+		else if(cmp = curr.compareTo(node) <= 0)
+			return insert(curr.left, node);
+		else
+			return insert(curr.right. node);
 	}
 	
 	public boolean empty() {
 		return root == null;
 	}
 	
-	
-	
-	public void populate(Node<E> curr,array[] data){
-		for(int i = 0; i < data.getSize(); i++) {
-			populate(curr.left);
-			data[i] = curr.data;
-			populate(curr.right);
-		}
-	
     public void remove(E key) {
-        
+        ...
     }
 	
 	private Node<E> remove(Node<E> curr, E key)
+	
+	
 
   	public boolean search(E key) {
-    return search(root, key);
+		return search(root, key);
 	}
 	
 	private boolean search(Node<E> curr,E key) {
-    Node<E> curr = root;
-    int cmp;
-    if(curr == null)
-		return false;
-	if ((cmp = key.compareTo(curr.data)) == 0) 
-        return true;
-    else if (cmp < 0)
-        search(curr.left, key);
-    else {
-        search(curr.right, key);
-        }
+		Node<E> curr = root;
+		int cmp;
+		if(curr == null)
+			return false;
+		if ((cmp = key.compareTo(curr.data)) == 0) 
+			return true;
+		else if (cmp < 0)
+			search(curr.left, key);
+		else
+			search(curr.right, key);
+        
     }
 	
 }
